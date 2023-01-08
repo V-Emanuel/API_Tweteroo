fe258d41f2846e3182ca5690d2fb5cff5c53b919
V-Emanuel <gvictor567@gmail.com>
2023-01-07 21:37:58 -0300

    post sing-up para pegar dados do usuário terminado

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import express from "express";
 import cors from "cors";
 
+const PORT = 5000;
 const app = express();
-const port = 5000;
-app.listen(port);
+app.use(cors());
+app.use(express.json());
+app.listen(PORT);
+const user = [];
 
+app.post('/sign-up', (req, res) => {
+
+    const dataUser = req.body
+    user.push(dataUser)
+    res.status(201).send("OK")
+
+})
+
+app.get('/sign-up', (req, res) => {
+
+    res.send(user)
+
+})
 
 
